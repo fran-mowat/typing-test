982309fde74aa416ba48f7abecfe538106aa0b51
fran0770 <franmowat33@gmail.com>
2024-03-02 18:15:48 +0000

    development of countdown timer

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ let setTimer = () => {
 
 let duration = document.getElementById("duration");
 duration.addEventListener("change", setTimer);
+
+let countdown = () => {
+    let now = new Date().getTime();
+    let difference = now - startTime;
+    if (difference < 0){
+        clearInterval(countdown, 100);
+    } else {
+        let minutes = 0 - Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = 59 - Math.floor((difference % (1000 * 60)) / (1000));
+
+        if (seconds < 10){
+            seconds = "0" + seconds;
+        }
+
+        clock.innerHTML = `0${minutes}:${seconds}`;
+    }
+}
+
+let startTime = new Date().getTime();
+setInterval(countdown, 100);
