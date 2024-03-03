0a04e358026a7741cfb2a6048f0d29a88b3460ec
fran0770 <franmowat33@gmail.com>
2024-03-03 11:58:13 +0000

    countdown reset on selection from dropdown

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
+let secondsToMinutes = (seconds) => {
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60; 
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    return [minutes, seconds];
+}
+
 let setTimer = () => {
     let clock = document.getElementById("clock");
-    clock.innerHTML = duration.value; 
+    let set_duration = secondsToMinutes(duration.value);
+    let set_minutes = set_duration[0];
+    let set_seconds = set_duration[1];
+
+    clock.innerHTML = `0${set_minutes}:${set_seconds}`;
+
+    clock.startTime = new Date().getTime();
+    let interval_ID = setInterval(countdown, 100);
+    clock.interval_ID = interval_ID;
 }
 
 let duration = document.getElementById("duration");
 duration.addEventListener("change", setTimer);
 
 let countdown = () => {
+    console.log("chec");
     let now = new Date().getTime();
-    let difference = now - startTime;
-    if (difference < 0){
-        clearInterval(countdown, 100);
+    let difference = now - clock.startTime;
+    if (difference > duration.value * 1000){
+        clearInterval(clock.interval_ID);
+        clock.innerHTML = "00:00";
     } else {
-        let minutes = 0 - Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = 59 - Math.floor((difference % (1000 * 60)) / (1000));
+        let seconds_passed = Math.floor((difference % (1000 * 60)) / (1000));
+        let seconds_remaining = duration.value - seconds_passed;
 
-        if (seconds < 10){
-            seconds = "0" + seconds;
-        }
+        let duration_remaining = secondsToMinutes(seconds_remaining);
+        let clock_minutes = duration_remaining[0];
+        let clock_seconds = duration_remaining[1];
 
-        clock.innerHTML = `0${minutes}:${seconds}`;
+        clock.innerHTML = `0${clock_minutes}:${clock_seconds}`;
     }
 }
 
-let startTime = new Date().getTime();
-setInterval(countdown, 100);
